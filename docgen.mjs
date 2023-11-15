06486760b39bcab5a4543c5c30fb983c41b6e3e5
Matej Vobornik <vobornik.matej@gmail.com>
2023-11-15 03:13:42 +0100

    Don't generate react prop types

--- docgen.mjs
+++ docgen.mjs
@@ -1,13 +1,17 @@
 import docgen from "react-docgen-typescript";
 import path from "node:path";
-import { fileURLToPath } from 'node:url'
+import {fileURLToPath} from "node:url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const result1 = docgen.parse(path.resolve(__dirname, "./Button.tsx"));
+const result1 = docgen.parse(path.resolve(__dirname, "./Button.tsx"), {
+  propFilter: (prop) => !prop.parent?.fileName.includes("node_modules"),
+});
 
 console.log(`Generated props for Button: ${Object.keys(result1[0].props).join(", ")}`);
 
-const result2 = docgen.parse(path.resolve(__dirname, "./ButtonStyled.tsx"));
+const result2 = docgen.parse(path.resolve(__dirname, "./ButtonStyled.tsx"), {
+  propFilter: (prop) => !prop.parent?.fileName.includes("node_modules"),
+});
 
 console.log(`Generated props for ButtonStyled: ${Object.keys(result2[0].props).join(", ")}`);
